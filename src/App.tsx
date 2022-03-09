fd3d69e99aa962642eb2ddd7c859503100595d6f
AlexPron <Fotolinza@gmail.com>
2022-03-09 22:33:13 +0300

    useState_1.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-    let a = 1
+    // let a = 1
+    let[a, setA] = useState (1)
     const onClickHandler = () => {
-        ++a
+        setA(++a)
         console.log(a)
     }
+
+    const onZeroHandler = () => {
+        setA(0)
+        console.log(a)
+    }
+
     return (
         <div className="App">
             <h1>{a}</h1>
             <button onClick={onClickHandler}>number</button>
+            <button onClick={onZeroHandler}>0</button>
         </div>
     )
         ;
