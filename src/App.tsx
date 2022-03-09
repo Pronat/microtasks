a29e329be286a706ec51a645f1dcdbf405134c38
AlexPron <Fotolinza@gmail.com>
2022-03-09 21:24:13 +0300

    button_1.4

--- src/App.tsx
+++ src/App.tsx
@@ -11,10 +11,15 @@ function App() {
         console.log(subscriber, age)
     }
 
+    const Button3Foo = (subscriber:string) => {
+        console.log(subscriber)
+    }
+
     return (
         <div className="App">
-            <Button name={"MyYouTubeChannel-1"} callBack={()=>Button1Foo( 'I am Vasa', 21, 'live in Minsk')}/>
+            <Button name={"MyYouTubeChannel-1"} callBack={()=>Button1Foo( 'I am Vasia', 21, 'live in Minsk')}/>
             <Button name={"MyYouTubeChannel-2"} callBack={()=>Button2Foo('I am Ivan',27)}/>
+            <Button name={"Stuped Button"} callBack={()=>Button3Foo('I am Stuped Button')}/>
 
         </div>
     )
