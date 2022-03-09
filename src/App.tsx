145ed49a7f6eaf51747428c704ca343007f42bf1
AlexPron <Fotolinza@gmail.com>
2022-03-09 22:09:22 +0300

    useState_1.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
 import './App.css';
-import {Button} from "./components/Button";
-
 
 function App() {
-    const Button1Foo = (subscriber:string,age:number, address:string) => {
-        console.log(subscriber, age, address)
+    let a = 1
+    const onClickHandler = () => {
+        ++a
+        console.log(a)
     }
-    const Button2Foo = (subscriber:string,age:number) => {
-        console.log(subscriber, age)
-    }
-
-    const Button3Foo = (subscriber:string) => {
-        console.log(subscriber)
-    }
-
     return (
         <div className="App">
-            <Button name={"MyYouTubeChannel-1"} callBack={()=>Button1Foo( 'I am Vasia', 21, 'live in Minsk')}/>
-            <Button name={"MyYouTubeChannel-2"} callBack={()=>Button2Foo('I am Ivan',27)}/>
-            <Button name={"Stuped Button"} callBack={()=>Button3Foo('I am Stuped Button')}/>
-
+            <h1>{a}</h1>
+            <button onClick={onClickHandler}>number</button>
         </div>
     )
         ;
