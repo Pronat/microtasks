04294f670fccd6a85e9446624198248db2aad4b6
AlexPron <Fotolinza@gmail.com>
2022-03-09 23:45:28 +0300

    filter_1.1

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,28 @@ import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-    // let a = 1
-    let[a, setA] = useState (1)
-    const onClickHandler = () => {
-        setA(++a)
-        console.log(a)
-    }
-
-    const onZeroHandler = () => {
-        setA(0)
-        console.log(a)
-    }
-
+    const [money, setMoney] = useState([
+        {banknots: 'Dollars', value: 100, number: ' a1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' z1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
+        {banknots: 'Dollars', value: 100, number: ' e1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' c1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' x1234567890'},
+        {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
+    ])
     return (
-        <div className="App">
-            <h1>{a}</h1>
-            <button onClick={onClickHandler}>number</button>
-            <button onClick={onZeroHandler}>0</button>
-        </div>
-    )
-        ;
+        <ul>
+            {money.map((objFromMoneyArray) => {
+                return (
+                    <li>
+                        <span>{objFromMoneyArray.banknots}</span>
+                        <span>{objFromMoneyArray.value}</span>
+                        <span>{objFromMoneyArray.number}</span>
+                    </li>
+                )
+            })}
+        </ul>
+    );
 }
-
-
-export default App;
+    export default App;
