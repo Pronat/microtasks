daa6a4d72a0e92cdbf18a557be7168cf23d4bdc5
AlexPron <Fotolinza@gmail.com>
2022-03-24 22:41:34 +0300

    input 2-2

--- src/App.tsx
+++ src/App.tsx
@@ -11,16 +11,24 @@ function App() {
         {message: 'message3'},
     ])
 
+    let [title, setTitle] = useState('')
+    console.log(title)
+
     const addMessage = (title: string) => {
         let newMessage = {message: title};
         setMessage([newMessage, ...message,])
     }
 
+    const callBackButtonHandler=()=>{
+        addMessage(title)
+        setTitle('')
+    }
+
     return (
         <div className={"App"}>
 
-            <Input />
-            <Button name={'+'} callBack={()=>{}} />
+            <Input setTitle={setTitle} title={title}/>
+            <Button name={'+'} callBack={callBackButtonHandler} />
 
             {message.map((el, index) => {
                 return (
