12212b37b00ba1000d17210a27a8406a85add864
AlexPron <Fotolinza@gmail.com>
2023-02-09 20:05:16 +0300

    button-3

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,13 @@ import {Button} from "./components/Button";
 
 
 function App() {
-    const onclickHandler = (name: string) => {
-        console.log(`Hello my friend ${name}`)
-    }
-    const whatTimeHandler = (name: string) => {
-        console.log(`Date: ${name}`)
-    }
 
 
 
     return (
         <div className={"App"}>
-           <Button title={'1'} callBack={ () => {onclickHandler('Vasya')}} />
-           <Button title={'2'} callBack={ () => {whatTimeHandler('2023')}} />
+
+
         </div>
     );
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,20 +1,16 @@
 import React from 'react';
 
 type ButtonType = {
-    title: string
-    callBack: () => void
+
 }
 
 export const Button = (props: ButtonType) => {
-        const onclickHandler = () => {
-                props.callBack()
-        }
-    return (
-        <div>
-            <span>
-                  <button onClick={onclickHandler}>{props.title}</button>
-            </span>
+      return (
+          <div>
 
-        </div>
-    );
+          </div>
+      )
+
+
+
 };
