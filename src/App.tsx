6b5ac06689d54ebd91beb1d703225bf69943ea80
AlexPron <Fotolinza@gmail.com>
2023-02-10 00:04:28 +0300

    button-13

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React, {useState} from 'react';
 import {log} from "util";
 import {Button} from "./components/Button";
+import {NewComponent} from "./components/NewComponent";
 
-
+export type FilterType = 'all' | 'Dollars' | 'RUBLS'
+export type MoneyType = {
+    banknots: string
+    value: number
+    number: string
+}
 function App() {
-    const [money, setMoney] = useState([
+    const [money, setMoney] = useState<Array<MoneyType>>([
         {banknots: 'Dollars', value: 100, number: ' a1234567890'},
         {banknots: 'Dollars', value: 50, number: ' z1234567890'},
         {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
@@ -16,7 +22,7 @@ function App() {
     ])
 
 
-    const [filter, setFilter] = useState('all')
+    const [filter, setFilter] = useState<FilterType>('all')
     let newMoney = money
     if (filter === 'Dollars') {
         newMoney = money.filter((el) => el.banknots === 'Dollars')
@@ -25,39 +31,43 @@ function App() {
         newMoney = money.filter((el) => el.banknots === 'RUBLS')
     }
 
-    const showMeHandler = (title: string) => {
+    const showMeHandler = (title: FilterType) => {
         setFilter(title)
         }
 
 
         return (
-            <div className={"App"}>
-                <div>
-                    {
-                        newMoney.map((el, index) => {
-                            return (
-                                <div key={index}>
-                                    <span>{el.value} </span>
-                                    <span>{el.banknots}</span>
-                                    <span>{el.number} </span>
-                                </div>
-
-                            )
-                        })
-                    }
-                </div>
-                <Button title={'all'} callBack={() => {
-                    showMeHandler('all')
-                }}/>
-                <Button title={'Dollars'} callBack={() => {
-                    showMeHandler('Dollars')
-                }}/>
-                <Button title={'RUBLS'} callBack={() => {
-                    showMeHandler('RUBLS')
-                }}/>
-
-
-            </div>
+            <NewComponent
+                newMoney={newMoney}
+                showMeHandler={showMeHandler}
+            />
+            // <div className={"App"}>
+            //     <div>
+            //         {
+            //             newMoney.map((el, index) => {
+            //                 return (
+            //                     <div key={index}>
+            //                         <span>{el.value} </span>
+            //                         <span>{el.banknots}</span>
+            //                         <span>{el.number} </span>
+            //                     </div>
+            //
+            //                 )
+            //             })
+            //         }
+            //     </div>
+            //     <Button title={'all'} callBack={() => {
+            //         showMeHandler('all')
+            //     }}/>
+            //     <Button title={'Dollars'} callBack={() => {
+            //         showMeHandler('Dollars')
+            //     }}/>
+            //     <Button title={'RUBLS'} callBack={() => {
+            //         showMeHandler('RUBLS')
+            //     }}/>
+            //
+            //
+            // </div>
         );
     }
     export default App;
