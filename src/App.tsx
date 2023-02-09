856fbfd52103d2faf55c8ca78545cc9db8b5568a
AlexPron <Fotolinza@gmail.com>
2023-02-09 18:58:55 +0300

    map-7

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,28 @@ function App() {
     // const HelloKolya = (event: MouseEvent<HTMLButtonElement>) => {
     //     console.log('Hello my friend Kolya')
     // }
-    const HelloKolya = (name: string) => {
-        console.log((name))
+    // const HelloKolya = (name: string) => {
+    //     console.log((name))
+    // }
+    const button1 = () => {
+        console.log("Button number 1")
     }
+
+    const button2 = (text: string) => {
+        console.log(text)
+    }
+
 
     return (
         <div className={"App"}>
-            <button onClick={() => {
-                console.log('Hello')
-            }}>click
-            </button>
-            <button onClick={() => {HelloKolya('Kolya')}}>button2</button>
+            {/*<button onClick={() => {*/}
+            {/*    console.log('Hello')*/}
+            {/*}}>click*/}
+            {/*</button>*/}
+            {/*<button onClick={() => {HelloKolya('Kolya')}}>button2</button>*/}
+            <button onClick={button1}>1</button>
+            <button onClick={()=>{button2('Button2')}}>2</button>
+
         </div>
     );
 }
