c6c96910837f83ce6861b1d2ec6a2c8603a06049
AlexPron <Fotolinza@gmail.com>
2023-02-09 20:11:56 +0300

    button-4

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {log} from "util";
 import {Button} from "./components/Button";
 
 
 function App() {
+    const [money, setMoney] = useState([
+        { banknots: 'Dollars', value: 100, number: ' a1234567890' },
+        { banknots: 'Dollars', value: 50, number: ' z1234567890' },
+        { banknots: 'RUBLS', value: 100, number: ' w1234567890' },
+        { banknots: 'Dollars', value: 100, number: ' e1234567890' },
+        { banknots: 'Dollars', value: 50, number: ' c1234567890' },
+        { banknots: 'RUBLS', value: 100, number: ' r1234567890' },
+        { banknots: 'Dollars', value: 50, number: ' x1234567890' },
+        { banknots: 'RUBLS', value: 50, number: ' v1234567890' },
+    ])
 
-
+    const showMeHandler = (currency: string) => {
+        console.log(`Sow me ${currency}`)
+    }
 
     return (
         <div className={"App"}>
-
+            <Button title={()=>{showMeHandler('Dollars')}}/>
+            <Button title={()=>{showMeHandler('RUBLS')}}/>
 
         </div>
     );
