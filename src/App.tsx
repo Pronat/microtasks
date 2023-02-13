a08d4f5b0d75ab12edfbb7e37d0400ef40954a60
AlexPron <Fotolinza@gmail.com>
2023-02-13 22:28:20 +0300

    input-2

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,7 @@
 import React, {useState} from 'react';
 
 
-// export type FilterType = 'all' | 'Dollars' | 'RUBLS'
-// export type MoneyType = {
-//     banknots: string
-//     value: number
-//     number: string
-// }
 function App() {
-    // const [money, setMoney] = useState<Array<MoneyType>>([
-    //     {banknots: 'Dollars', value: 100, number: ' a1234567890'},
-    //     {banknots: 'Dollars', value: 50, number: ' z1234567890'},
-    //     {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
-    //     {banknots: 'Dollars', value: 100, number: ' e1234567890'},
-    //     {banknots: 'Dollars', value: 50, number: ' c1234567890'},
-    //     {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
-    //     {banknots: 'Dollars', value: 50, number: ' x1234567890'},
-    //     {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
-    // ])
-
-    // const [filter, setFilter] = useState<FilterType>('all')
-    // let newMoney = money
-    // if (filter === 'Dollars') {
-    //     newMoney = money.filter((el) => el.banknots === 'Dollars')
-    // }
-    // if (filter === 'RUBLS') {
-    //     newMoney = money.filter((el) => el.banknots === 'RUBLS')
-    // }
-    //
-    // const showMeHandler = (title: FilterType) => {
-    //     setFilter(title)
-    //     }
-    //
-    //
-    //     return (
-    //         <NewComponent
-    //             newMoney={newMoney}
-    //             showMeHandler={showMeHandler}
-    //         />
-    //
-    //     );
-    // }
 
     let [message, setMessage] = useState([
         {message: 'message1'},
